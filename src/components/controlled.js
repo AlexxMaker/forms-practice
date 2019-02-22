fb6fc85b12ff3abc575eda7955cb6298d76ca7dd
AlexxMaker <Alexeus92@mail.ru>
2019-02-22 22:18:12 +0430

    Forms 2 F

--- src/components/controlled.js
+++ src/components/controlled.js
@@ -3,13 +3,44 @@ import React, { Component } from 'react';
 class Controlled extends Component {
 
     state = {
-        
+        name: '',
+        lastname: ''
+    }
+
+    handleNameChange = (event) => {
+        this.setState({
+            name:event.target.value
+        })
+    }
+
+    handleLastnameChange = (event) => {
+        this.setState({
+            lastname:event.target.value
+        })
+    }
+
+    onshandler = (event) => {
+        event.preventDefault();
+
+        console.log(this.state)
     }
 
     render(){
         return(
-            <div>
-                Controlled
+            <div className="container">
+                <form onSubmit ={this.onshandler}>
+                    <div className="form_element">
+                        <label>Enter Name</label>
+                        <input type="text" onChange={this.handleNameChange} value={this.state.name}></input>
+                    </div>
+
+                    <div className="form_element">
+                        <label>Enter Lastname</label>
+                        <input type="text" onChange={this.handleLastnameChange} value={this.state.lastname}></input>
+                    </div>
+
+                    <button type="submit">Submit</button>
+                </form>
             </div>
         )
     }
--- src/components/user.js
+++ src/components/user.js
@@ -1,18 +1,58 @@
 import React, { Component } from 'react';
+import FormFields from '../../src/widgets/Forms/formFields'
 
 class User extends Component {
 
     state = {
-        
+        formData: {
+            name:{
+                element:'input',
+                value:'',
+                label:true,
+                labelText:'Name',
+                config: {
+                    name: 'name_input',
+                    type: 'text',
+                    placeholder: 'Enter your Name'
+                }
+            },
+
+            lastname:{
+                element:'input',
+                value:'',
+                label:true,
+                labelText:'Lastname',
+                config: {
+                    name: 'lastname_input',
+                    type: 'text',
+                    placeholder: 'Enter your Lastname'
+                    }
+            
+            }
+        }
+    }
+
+    updateForm = (newState) => {
+        console.log(newState)
     }
 
     render(){
         return(
-            <div>
-                User
+            <div className="container">
+                <form onSubmit={this.submitForm}>
+
+                <FormFields 
+                    formData={this.state.formData}
+                    change={(newState) => this.updateForm(newState)}
+                
+                />
+
+                    <button type="submit">Submit</button>
+                </form>
             </div>
         )
     }
 }
+
 
 export default User;
