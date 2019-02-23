ad445e91c8b41e24d395196d2de9448d0731f53f
AlexxMaker <Alexeus92@mail.ru>
2019-02-23 20:14:32 +0430

    Forms finished. FUUUUUCK!

--- src/widgets/Forms/formFields.js
+++ src/widgets/Forms/formFields.js
@@ -27,13 +27,56 @@ const FormFields = (props) => {
         : null
     }
 
-    const changeHandler = (event, id) => {
+    const changeHandler = (event, id, blur) => {
         const newState = props.formData;
 
         newState[id].value = event.target.value;
 
+        if(blur){
+            let validData = validate(newState[id])
+            newState[id].valid = validData[0];
+            newState[id].validationMessage = validData[1];
+        }
+        newState[id].touched = blur;
+
         props.change(newState)
 
+    }
+
+    const validate = (element) => {
+        let error = [true, '']
+
+
+        if(element.validation.minLen){
+            const valid = element.value.length >= element.validation.minLen;
+            const message = `${!valid ? 'Must be greater than ' + element.validation.minLen :''}`
+
+            error = !valid ? [valid, message] : error
+        }
+
+        if(element.validation.required) {
+            const valid = element.value.trim() !== '';
+            const message = `${!valid ? 'This field is required':''}`
+
+            error = !valid ? [valid, message] : error
+        }
+
+
+
+        return error;
+    }
+
+    const showValidation = (data) => {
+        let errorMessage = null;
+
+        if(data.validation  && !data.valid){
+            errorMessage = (
+                <div className="label_error">
+                    {data.validationMessage}
+                </div>
+            )
+        }
+        return errorMessage;
     }
 
     const renderTemplates = (data) => {
@@ -47,13 +90,51 @@ const FormFields = (props) => {
                     { showLabel(values.label,values.labelText) }
                     <input {...values.config} 
                             value={values.value}
+                            onBlur={
+                                (event) => changeHandler(event, data.id, true)
+                            }
                             onChange={
-                                (event) => changeHandler(event, data.id)
+                                (event) => changeHandler(event, data.id, false)
                             }
                     />
+                    {showValidation(values)}
                 </div>
                 )
             break;
+
+            case('textarea'):
+                formTemplate = (
+                    <div>
+                        { showLabel(values.label,values.labelText) }
+                        <textarea {...values.config}
+                                value={values.value}
+                                onChange={
+                                    (event) => changeHandler(event, data.id)
+                                }
+                                />
+                    </div>
+                )
+                break;
+
+            case('select'):
+                formTemplate = (
+                    <div>
+                        { showLabel(values.label,values.labelText) }
+                        <select
+                            value={values.value}
+                            name={values.config.name}
+                            onChange={
+                                (event) => changeHandler(event, data.id)
+                            }>
+                        {values.config.options.map((item, i) => (
+                            <option key={i} value={item.val}>{item.text}</option>
+                        )) }
+                        </select>
+                    </div>
+
+                )
+                break;
+
             default:
             formTemplate = null
         }
